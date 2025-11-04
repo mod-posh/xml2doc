e7ad2c37bbecef1fc8d6c4dec3c11c15db5d2130
Jeffrey Patton <jeffrey@patton-tech.com>
2025-11-04 11:59:41 -0600

    Shims to support multiple frameworks

---
+++ Xml2Doc/src/Xml2Doc.Core/Compat/NetStandard20Compat.cs
@@ -0,0 +1,21 @@
+﻿#if NETSTANDARD2_0
+using System;
+
+namespace Xml2Doc.Core.Compat
+{
+    internal static class Net20Compat
+    {
+        // Replace s[a..b] with Slice(s, a, b)
+        public static string Slice(string s, int start, int end)
+            => s.Substring(start, end - start);
+
+        // Replace s[^1] with FromEnd(s, 1)
+        public static char FromEnd(string s, int indexFromEnd)
+            => s[s.Length - indexFromEnd];
+
+        // Replace an expression like s.AsSpan(i, len) with Substr(s, i, len)
+        public static string Substr(string s, int start, int len)
+            => s.Substring(start, len);
+    }
+}
+#endif
---
+++ Xml2Doc/src/Xml2Doc.Core/IsExternalInit.cs
@@ -0,0 +1,3 @@
+﻿#if !NET5_0_OR_GREATER
+namespace System.Runtime.CompilerServices { internal static class IsExternalInit { } }
+#endif
